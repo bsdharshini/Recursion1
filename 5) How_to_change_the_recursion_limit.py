de37ffd24fa8ce38c6282a99f1a0693b00a53b1b
Dharshini BS <44402281+bsdharshini@users.noreply.github.com>
2022-04-30 18:55:19 +0530

    How_to_change_the_recursion_limit

---
+++ 5) How_to_change_the_recursion_limit.py
@@ -0,0 +1,9 @@
+#How_to_change_the_recursion_limit
+
+import sys as s
+print(s.recursionlimit()) # print the default limit
+s.setrecursionlimit(3000)# sets new limit
+def limits():
+  print('hello')
+  limits() # prints least 3000 times
+limits()
